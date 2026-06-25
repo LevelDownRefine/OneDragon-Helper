bc3734d09d468b5681be8ad87d1a3dde8aedebf3
winsanyu <973530837@qq.com>
2026-06-25 17:17:25 +0800

    refactor: add gen_onedragon_config.py and utils.py

---
+++ gen_onedragon_config.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+from utils import get_path_under_onedragon, get_path_under_cwd
+from config_ui import run_config_ui
+
+CHAIN_NAME = "01.yml"
+
+def is_config_exist():
+    config_path = get_path_under_onedragon("config", "script_chain")
+    return os.path.exists(os.path.join(config_path, CHAIN_NAME))
+
+def copy_all_configs():
+    output_dir = get_path_under_onedragon("config", "script_chain")
+    os.makedirs(output_dir, exist_ok=True)
+    for i in range(1, 8):
+        chain_name = f"{i:02d}.yml"
+        input_path = get_path_under_cwd(chain_name)
+        output_path = os.path.join(output_dir, chain_name)
+        if os.path.exists(input_path):
+            shutil.copy(input_path, output_path)
+            print(f"已复制配置文件到: {output_path}")
+
+def copy_python_scripts():
+    py_script_dir = get_path_under_cwd("python_script")
+    file_names = [f for f in os.listdir(py_script_dir) if f.endswith(".py")]
+    for file_name in file_names:
+        input_path = os.path.join(py_script_dir, file_name)
+        output_path = get_path_under_onedragon("config", "script_chain", "scripts")
+        if not os.path.exists(os.path.join(output_path, file_name)):
+            shutil.copy(input_path, output_path)
+            print(f"已复制Python脚本{file_name}到: {output_path}")
+
+def config_workflow():
+    copy_python_scripts()
+    run_config_ui(CHAIN_NAME)
+    copy_all_configs()
+
+if __name__ == "__main__":
+    config_workflow()
---
+++ utils.py
@@ -0,0 +1,44 @@
+import os
+
+BaseDIR = os.path.dirname(os.path.abspath(__file__))
+OneDragonScriptChainerDir = os.path.join(BaseDIR, "OneDragon-ScriptChainer")
+
+def get_path_under_onedragon(*subs) -> str:
+    """
+    获取工作目录下的路径
+    :param subs: 子目录路径 可以传入多个表示多级
+    :return: 工作目录下的路径
+    """
+    return join_dir_path_with_mk(OneDragonScriptChainerDir, *subs)
+
+def get_cwd() -> str:
+    """
+    获取当前工作目录
+    :return: 当前工作目录
+    """
+    return os.path.dirname(os.path.abspath(__file__))
+
+def get_path_under_cwd(*subs) -> str:
+    """
+    获取当前工作目录下的路径
+    :param subs: 子目录路径 可以传入多个表示多级
+    :return: 当前工作目录下的路径
+    """
+    return join_dir_path_with_mk(get_cwd(), *subs)
+
+def join_dir_path_with_mk(path: str, *subs) -> str:
+    """
+    拼接目录路径和子目录
+    如果拼接后的目录不存在 则创建
+    :param path: 目录路径
+    :param subs: 子目录路径 可以传入多个表示多级
+    :return: 拼接后的子目录路径
+    """
+    target_path = path
+    for sub in subs:
+        if sub is None:
+            continue
+        target_path = os.path.join(target_path, sub)
+        if not os.path.exists(target_path):
+            os.mkdir(target_path)
+    return target_path
