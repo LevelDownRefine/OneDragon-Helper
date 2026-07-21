250be43a371afc38a8d437f2d4f31736dfd031d9
winsanyu <973530837@qq.com>
2026-07-21 15:08:47 +0800

    update pycaw

--- src/python_script/mute.py
+++ src/python_script/mute.py
@@ -1,15 +1,11 @@
-from pycaw.pycaw import AudioUtilities
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from ctypes import cast, POINTER
 
 def set_mute(mute_status):
-    # 1. 获取设备包装对象
     devices = AudioUtilities.GetSpeakers()
-
-    # 2. 直接访问已经封装好的 EndpointVolume 属性
-    interface = devices.EndpointVolume
-
-    # 3. 执行静音 (True 为 1，False 为 0)
+    interface = cast(devices.Activate(
+        IAudioEndpointVolume._iid_, 0, None), POINTER(IAudioEndpointVolume))
     interface.SetMute(mute_status, None)
-
     print(f"系统已{'静音' if mute_status else '恢复声音'}")
 
 if __name__ == "__main__":
